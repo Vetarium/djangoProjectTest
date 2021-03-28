093f0440871cb583118d3d2820ece5a63f4303d3
Vetarium <aronbestaron@gmail.com>
2021-03-28 14:14:11 +0600

    added models

--- myPosts/models.py
+++ myPosts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=70)
+    password = models.CharField(max_length=70)
+    name = models.CharField(max_length=70)
+    loginTime = models.DateField()
+
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    createDate = models.DateField(auto_now=True)
+    title = models.CharField(max_length=70)
+    text = models.TextField()
+    likes = models.IntegerField()
--- myPosts/views.py
+++ myPosts/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    context = {
+
+    }
+    template = 'index.html'
+    return render(request, template, context)
