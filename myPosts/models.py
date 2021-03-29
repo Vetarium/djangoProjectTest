b7b8fcd4653537e7086d2d85842764ab7bf3e130
Vetarium <aronbestaron@gmail.com>
2021-03-30 00:41:53 +0600

    register and login pages

--- myPosts/models.py
+++ myPosts/models.py
@@ -1,17 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib import auth
+
+
 
 # Create your models here.
-class User(models.Model):
-    email = models.CharField(max_length=70)
-    password = models.CharField(max_length=70)
-    name = models.CharField(max_length=70)
-    loginTime = models.DateField()
-
 
 
 class Post(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    createDate = models.DateField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    createDate = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=70)
     text = models.TextField()
     likes = models.IntegerField()
+
+    def __str__(self):
+        return '%s %s %s likes %s' % (self.title, self.text, self.likes, self.createDate)
+    def like(likes):
+        likes = likes +1
+
+
+
--- myPosts/views.py
+++ myPosts/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
+from .models import Post
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
+    list_posts = Post.objects.all()
     context = {
+        'list_posts': list_posts
 
     }
     template = 'index.html'
+    return render(request, template, context)
+
+def login(request):
+    context = {
+
+    }
+    template = 'login.html'
+    return render(request, template, context)
+
+def register(request):
+    form = UserCreationForm()
+    context = {
+        'form' : form
+    }
+    template = 'register.html'
     return render(request, template, context)
